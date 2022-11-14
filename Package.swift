47b77b56d6fd9e16f6741aa5db4fc16fbfbb3ec4
Josh Wright <joshuawright11@gmail.com>
2022-11-14 15:57:23 -0800

    WIP

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "sardine",
-            targets: ["sardine"]),
+            targets: ["MobileIntelligence"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,11 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "sardine",
-            dependencies: []),
-        .testTarget(
-            name: "sardineTests",
-            dependencies: ["sardine"]),
+        .binaryTarget(name: "MobileIntelligence", path: "MobileIntelligence-1.0.15.xcframework"),
     ]
 )
